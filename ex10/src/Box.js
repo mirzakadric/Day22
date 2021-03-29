7c67ecefd3831108d932bf6576dc329e4b3d8662
Mirza Kadric <kadric.mirza@hotmail.com>
2021-03-29 23:48:47 +0200

    And finally ex10

---
+++ ex10/src/Box.js
@@ -0,0 +1,11 @@
+// example simple component
+
+import React, { Component } from "react";
+
+function Box(props) {
+    // Only change code below this line
+    return <h2>This is a {props.size}px Box</h2>
+    // Only change code above this line
+}
+
+export default Box;
