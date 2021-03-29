822c5f3591d1d17c4bf6da2c122a1e22e6e7b329
Mirza Kadric <kadric.mirza@hotmail.com>
2021-03-29 23:07:02 +0200

    Ex07

---
+++ ex07/src/Box.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Box extends Component {
+    render() {
+        return(
+            <h2>This is a Box Component!</h2>
+        )
+        
+    }
+}
+
+export default Box;
